26adb4daaab5eec3e08973f19baf9b7178b4348e
SrikantSahoo <ss0049355@gmail.com>
2016-04-05 23:41:43 +1000

    code cleanup

--- app/app.js
+++ app/app.js
@@ -20,7 +20,6 @@
 	angular
 		.module("ePayroll",[
 			'ngRoute',
-			'lokijs',
 			'ngCsvImport',
 			'ngSanitize', 
 			'ngCsv',
--- app/payroll/taxtable/taxtable.js
+++ app/payroll/taxtable/taxtable.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ePayroll.taxtable',['ngRoute','lokijs'])
+angular.module('ePayroll.taxtable',['ngRoute'])
 .config(['$routeProvider',function($routeProvider){
 	$routeProvider.
 		when('/taxtable',{
@@ -10,45 +10,6 @@ angular.module('ePayroll.taxtable',['ngRoute','lokijs'])
 		});
 }])
 
-.controller('taxtableController',['$scope', 'Loki', function($scope,Loki){
-
-  var idbAdapter = new LokiIndexedAdapter('ePayroll');
-  var db = new loki('taxslabs', { persistenceMethod: 'adapter', adapter: myAdapter });
-    $scope.taxslabs = {};
-    $scope.taxslab = {};
-
-    // -------- Loki database instance ----------
-    // db = new Loki('taxtable.json');
-
-    // ------- Chargement collections --------
-    db.loadDatabase({}, function() {
-      // --- taxslabs collection -----
-      $scope.taxslabs = db.getCollection('  ') || db.addCollection('taxslabs');
-    });
-
-    // ---------- Scope instance -------------
-    $scope.addTaxslab = function() {
-      
-      $scope.taxslabs.insert({
-        min_income: $scope.taxslab.min_income,
-        max_income: $scope.taxslab.max_income,
-        tax_over: $scope.taxslab.tax_over,
-        tax_amount: $scope.taxslab.tax_amount,
-        taxplus_perdollor: $scope.taxslab.taxplus_perdollor,
-        rate_startyear: $scope.taxslab.rate_startyear,
-        rate_endyear: $scope.taxslab.rate_endyear,
-        effective_date: $scope.taxslab.effective_date,
-        status: $scope.taxslab.status
-      });
-      $scope.taxslab = {};
-      
-      db.saveDatabase();
-
-
-    };
-
-    $scope.collections = db.listCollections();
-    console.log($scope.collections);
-
+.controller('taxtableController',['$scope', function($scope){
 
 }]);
